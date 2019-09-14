55bbeed4d85e3fc067e2cd229cb138eca4c4c6b8
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-09-15 00:50:49 +0700

    Create slowlybyte.py

---
+++ slowlybyte.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# SlowlyByte - Web Server
+# Version 1.0 ( Alpha )
+# Copyright (c)2019 - Afrizal F.A - IN CRUST WE RUSH
+
+import os, re, sys, socket
+
+port=int(sys.argv[1])
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("0.0.0.0", port))
+s.listen(True)
+
+while True :
+	c,a=s.accept()
+	data=c.recv(1024)
+	line_headers=data.split("\n")
+	print(data)
+	open_file=re.findall("/(.+?) HTTP/" ,line_headers[0])
+	if not open_file :
+		if os.path.isfile("index.html") :
+			open_file=["index.html"]
+		if os.path.isfile("index.php") :
+			open_file=["index.php"]
+	if open_file :
+		split_ext=open_file[0].split(".")[-1]
+	else :
+		content_type="text/html"
+	if str(split_ext) :
+		if str(split_ext) == "html" or str(split_ext) == "php" or str(split_ext) == "phtml" :
+			content_type="text/html"
+		elif str(split_ext) == "txt" or str(split_ext) == "css" or str(split_ext) == "js":
+			content_type="text/plain"
+		elif str(split_ext) == "mp4" :
+			content_type="video/mp4"
+		elif str(split_ext) == "mp3" :
+			content_type="audio/mp3"
+		else :
+			content_type="application/octet-stream"
+	else :
+		content_type="text/html"
+		
+	h_ok="""\nHTTP/1.1 200 OK\nServer: SlowlyByte ( IN CRUST WE RUSH )\nConnection: Close\nContent-Type: %s\n\n"""%(content_type)
+	h_notfound="""\nHTTP/1.1 404 Not Found\nServer: SlowlyByte ( IN CRUST WE RUSH )\nConnection: Close\nContent-Type: text/html\n\n404 Not Found"""
+	try :
+		if open_file :
+			if os.path.isfile(open_file[0]) :
+				file_content=open(open_file[0], "r").read()
+				c.sendall(h_ok+file_content)
+			elif os.path.isdir(open_file[0]) :
+				if os.path.isfile(open_file[0]+"/index.html") :
+					open_file=[open_file[0]+"/index.html"]
+				if os.path.isfile(open_file[0]+"/index.php") :
+					open_file=[open_file[0]+"/index.php"]
+				c.sendall("HTTP/1.1 200 OK\nServer: SlowlyByte ( IN CRUST WE RUSH )\nConnection: Close\nContent-Type: text/html\n\n"+open(open_file[0], "r").read())
+			else :
+				c.sendall(h_notfound)
+		else :
+			c.sendall(h_notfound)
+	except :
+		pass
